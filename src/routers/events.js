b9918a4cde00db1a9dcd518047ca26531d43eddb
vins-stha <shrs_samar@yahoo.com>
2022-06-19 18:38:37 +0300

    routes for events

--- src/routers/events.js
+++ src/routers/events.js
@@ -1,5 +1,14 @@
-import express from 'express'
+import express from 'express';
+import {
+    getAll, getFinalResultsOfEvent, findEventById, addNewEvent, deleteEventById, updateEventById, addVote
+} from '../controllers/events'
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', findA)
+router.get('/', getAll);
+router.get('/:eventId', findEventById);
+router.post('/', addNewEvent);
+router.put('/:eventId', updateEventById);
+router.delete('/:eventId', deleteEventById);
+router.post('/:eventId/vote', addVote);
+router.get('/:eventId/results', getFinalResultsOfEvent);
