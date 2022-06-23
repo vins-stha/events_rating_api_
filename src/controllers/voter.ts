4bd442b22e947191aee6d7db319b4752dd88660b
vins-stha <shrs_samar@yahoo.com>
2022-06-23 14:40:11 +0300

    module for voter

---
+++ src/controllers/voter.ts
@@ -0,0 +1,70 @@
+import { Request, Response, NextFunction } from "express";
+
+import Voter, { VoterDocument } from "../models/Voter";
+import VoterService from "../services/voter";
+import { BadRequestError } from "../helpers/apiError";
+
+// GET /voters
+export const findAll = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    res.json(await VoterService.findAll());
+  } catch (error) {
+    if (error instanceof Error && error.name == "ValidationError") {
+      next(new BadRequestError("Invalid Request", error));
+    } else {
+      next(error);
+    }
+  }
+};
+
+// POST /voters
+export const addVoter = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name } = req.body;
+
+    const voter = new Voter({
+      name: name,
+    });
+
+    await VoterService.create(voter);
+    res.json(voter);
+  } catch (error) {
+    if (error instanceof Error && error.name == "ValidationError") {
+      next(new BadRequestError("Invalid Request", error));
+    } else {
+      next(error);
+    }
+  }
+};
+
+// DELETE /voters/:voterId
+export const deleteVoterByName = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    await VoterService.deleteVoterByName(req.params.name);
+    res.status(204).end();
+  } catch (error) {
+    if (error instanceof Error && error.name == "ValidationError") {
+      next(new BadRequestError("Invalid Request", error));
+    } else {
+      next(error);
+    }
+  }
+};
+
+export const isNewVoter = (name: string): boolean => {
+  let list = VoterService.findAll();
+  let exist = list && list?.filter((n: string) => n === name);
+  return exist && exist.length > 0 ? true : false;
+};
